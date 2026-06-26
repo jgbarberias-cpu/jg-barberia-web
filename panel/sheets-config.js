cba6ef1e0ed8ccd56572e72c210d36bf831c60be
JG Barberia <jg.barberias@gmail.com>
2026-06-26 16:54:12 -0300

    Actualizar URL de Apps Script (deploy con dedup)

--- panel/sheets-config.js
+++ panel/sheets-config.js
@@ -1,3 +1,3 @@
 // Pegá aquí la URL de tu Google Apps Script (Implementar > Nueva implementación >
 // Aplicación web), termina en "/exec".
-window.PANEL_SHEETS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbwlETRp8u4HTRevvqPr0zDsDxXJ8R2ZDZujNP4vVf4Xj2fr_SruFwESc0ZMqzgZ76AN/exec';
+window.PANEL_SHEETS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbyEL6wU7g_1fX7NHjj1T3yDAGoOZk3HnfKqwt1k668caMAjeCfft_1mkPV2YJgVvyoL/exec';
