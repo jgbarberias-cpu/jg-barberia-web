87d543c0307b5199df32ea5aecab334b90d1fc3c
JG Barberia <jg.barberias@gmail.com>
2026-06-26 16:45:21 -0300

    Actualizar URL de Apps Script para sync de clientes

--- panel/sheets-config.js
+++ panel/sheets-config.js
@@ -1,3 +1,3 @@
 // Pegá aquí la URL de tu Google Apps Script (Implementar > Nueva implementación >
 // Aplicación web), termina en "/exec".
-window.PANEL_SHEETS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbyVtju7OQ0KE5lWDUg3i-vBTs_arq2M6JwP6vZ5toaB5ch7eDFZLXdhDKfme6iacvNy/exec';
+window.PANEL_SHEETS_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbwlETRp8u4HTRevvqPr0zDsDxXJ8R2ZDZujNP4vVf4Xj2fr_SruFwESc0ZMqzgZ76AN/exec';
